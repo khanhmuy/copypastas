ae3eb50d7ec94e77a4ab30d2db043612ed21c656
beerpsi <92439990+beerpiss@users.noreply.github.com>
2022-01-28 06:30:58 +0700

    Update compress.sh

--- compress.sh
+++ compress.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-find "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" -type f -not -name $(basename $0) -exec sh -c '
+find "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" -not \( -path ./.git -prune \) -type f -not -name $(basename $0) -exec sh -c '
   for f; do
     mimetype -b "$f" | grep -Eqv "(image)|(octet\-stream)|(gzip)" &&
-    brotli -Zkf $f && gzip -9kf $f
+    brotli -Zkf $f && gzip -n9kf $f
   done
 ' sh {} + 
