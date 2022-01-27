17698f29e606f573bc795e8512d53daaa6b47fda
beerpsi <92439990+beerpiss@users.noreply.github.com>
2022-01-28 06:40:50 +0700

    Update compress.sh

--- compress.sh
+++ compress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" -not \( -path \*.git -prune \) -not -name $(basename $0) -not -regex '*.yaml' -type f -exec sh -c '
+find "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" -not \( -path \*.git -prune \) -not \( -path \*.github -prune \) -not -name $(basename $0) -type f -exec sh -c '
   for f; do
     mimetype -b "$f" | grep -Eqv "(image)|(octet\-stream)|(gzip)" &&
     brotli -Zkf $f && gzip -n9kf $f
